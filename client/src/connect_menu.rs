fd8c635574c09d4437791652a5a14ba484dfbc36
Daniel Lyne <DLyne@pm.me>
2022-06-18 18:58:03 -0400

    Add client connect menu

---
+++ client/src/connect_menu.rs
@@ -0,0 +1,69 @@
+use std::{net::SocketAddr, str::FromStr};
+
+use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext};
+
+pub struct ConnectEvent {
+    pub socket_addr: SocketAddr,
+    pub username: String,
+    pub room_password: String,
+}
+
+#[derive(Default)]
+pub struct UiState {
+    error_msg: String,
+
+    socket_addr_s: String,
+
+    username: String,
+    password: String,
+}
+
+pub fn connect_menu_init(mut commands: Commands) {
+    commands.insert_resource(UiState::default());
+}
+
+pub fn connect_menu(
+    mut egui_context: ResMut<EguiContext>,
+    mut ui_state: ResMut<UiState>,
+    mut writer: EventWriter<ConnectEvent>,
+) {
+    let mut clicked = false;
+
+    egui::Window::new("Connect").show(egui_context.ctx_mut(), |ui| {
+        ui.horizontal(|ui| {
+            ui.label("Socket Address");
+            ui.text_edit_singleline(&mut ui_state.socket_addr_s);
+        });
+
+        ui.horizontal(|ui| {
+            ui.label("Username");
+            ui.text_edit_singleline(&mut ui_state.username);
+        });
+
+        ui.horizontal(|ui| {
+            ui.label("Room Password");
+            ui.text_edit_singleline(&mut ui_state.password);
+        });
+
+        clicked = ui.button("Connect").clicked();
+
+        ui.label(&ui_state.error_msg);
+    });
+
+    if clicked {
+        let socket_addr = SocketAddr::from_str(&ui_state.socket_addr_s);
+        if ui_state.username.is_empty() {
+            ui_state.error_msg = "You must enter a username".to_owned();
+        }
+
+        match socket_addr {
+            Ok(socket_addr) => writer.send(ConnectEvent {
+                socket_addr,
+                username: ui_state.username.clone(),
+                room_password: ui_state.password.clone(),
+            }),
+            Err(e) => ui_state.error_msg = e.to_string(),
+        }
+    }
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,30 @@
 // disable console on windows for release builds
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::{App, ClearColor, Color, Msaa, WindowDescriptor};
-use bevy::DefaultPlugins;
-use bevy_game::GamePlugin;
+use std::net::SocketAddr;
+
+use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use iyes_loopless::prelude::*;
+use naia_bevy_client::{ClientConfig, Plugin as ClientPlugin, Stage};
+
+use rgj_shared::{protocol::Protocol, shared_config, Channels};
+
+use rgj_client::connect_menu;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub enum GameState {
+    ConnectMenu,
+    WaitingForMoreConnectionsMenu,
+    CountdownMenu,
+    Game,
+}
+
+pub struct ConnectionInformation {
+    socket_addr: SocketAddr,
+    username: String,
+    room_password: String,
+}
 
 fn main() {
     App::new()
@@ -16,6 +37,41 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin)
+        .add_plugin(EguiPlugin)
+        .add_plugin(ClientPlugin::<Protocol, Channels>::new(
+            ClientConfig::default(),
+            shared_config(),
+        ))
+        .add_event::<connect_menu::ConnectEvent>()
+        .add_loopless_state(GameState::ConnectMenu)
+        // ConnectMenu state
+        .add_enter_system(GameState::ConnectMenu, connect_menu::connect_menu_init)
+        .add_system_set(
+            ConditionSet::new()
+                .run_in_state(GameState::ConnectMenu)
+                .with_system(connect_menu::connect_menu)
+                .with_system(monitor_for_addr)
+                .into(),
+        )
+        //.add_exit_system(GameState::ConnectMenu, destroy_ui)
         .run();
+}
+
+/// Monitors for an event from the [`connect_menu::connect_menu`] so the game knows to begin
+/// connection
+fn monitor_for_addr(mut commands: Commands, mut events: EventReader<connect_menu::ConnectEvent>) {
+    if let Some(connect_menu::ConnectEvent {
+        socket_addr,
+        username,
+        room_password,
+    }) = events.iter().next()
+    {
+        commands.insert_resource(ConnectionInformation {
+            socket_addr: *socket_addr,
+            username: username.clone(),
+            room_password: room_password.clone(),
+        });
+        commands.insert_resource(NextState(GameState::WaitingForMoreConnectionsMenu));
+        info!("HERE!");
+    }
 }
